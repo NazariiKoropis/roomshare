ea0a961e762d7e69fc779532580ce72883296a20
Nazarii Koropis <nazarkoropisprogramming@gmail.com>
2026-03-01 15:28:40 +0200

    update NotFound page

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,12 +1,30 @@
+//styles
+import styles from './NotFound.module.scss'
+
 //components
 import Container from '../../components/layout/container/Container'
 
+//libs
+import { ThreeDots } from 'react-loader-spinner'
+
 function NotFound() {
   return (
-    <Container>
-      {' '}
-      <h1>Сторінку не знайдено:(</h1>
-    </Container>
+    <section>
+      <Container styled={styles.wrapperNotFound}>
+        {' '}
+        <h1>Сторінку не знайдено:(</h1>
+        <ThreeDots
+          height="100"
+          width="100"
+          radius="9"
+          color="var(--accent-primary)"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{ margin: '20px' }}
+          wrapperClass="custom-loader"
+          visible={true}
+        />
+      </Container>
+    </section>
   )
 }
 
